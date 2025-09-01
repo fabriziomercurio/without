6f9e9e32afd682abd15d99b48ac3f603eecdf6fa
fabriziomercurio <mercurio.fabrizio@gmail.com>
2025-09-01 09:38:22 +0200

    env class created for environment variables

--- src/App/Core/Connections/MySQL.php
+++ src/App/Core/Connections/MySQL.php
@@ -3,13 +3,16 @@ declare(strict_types=1);
 
 namespace App\Core\Connections; 
 use App\Interfaces\Database; 
+use App\Core\Env; 
 
 class MySQL implements Database
-{
+{   
     public static function connect() 
-    {
+    {   
+        ENV::getContent();
+
         try {
-            $conn = new \PDO('mysql:dbname=mvc;host='.'db', 'root', 'root'); 
+            $conn = new \PDO('mysql:dbname='.ENV::$config['DATABASE'].';host='.ENV::$config['HOST'], ENV::$config['USER'],ENV::$config['PASSWORD']); 
             return $conn; 
         } catch (\Throwable $th) {
              die("Impossibile stabilire una connessione al db " . $th->getMessage());
---
+++ src/App/Core/Env.php
@@ -0,0 +1,44 @@
+<?php 
+declare(strict_types=1); 
+
+namespace App\Core; 
+
+class Env 
+{  
+    public static Array $config = []; 
+
+    private static function fileGetContent(string $file) : string
+    {
+        return file_get_contents($file); 
+    } 
+
+    private static function setContent() 
+    {
+        $file = self::fileGetContent(".env"); 
+
+        $lines = explode("\n",$file); 
+
+        foreach ($lines as $line) {         
+            // Ignore lines starting with "#/*^" and others, or don't have a value after "="
+            preg_match('/\w+\=(.*)/',$line,$matches); 
+              
+            if(isset($matches[1])) 
+            {
+                putenv(trim($line)); 
+            }
+        }
+    } 
+
+    public static function getContent() 
+    {
+        $data = self::setContent();  
+        self::$config['HOST'] = getenv('HOST'); 
+        self::$config['USER'] = getenv('USER');
+        self::$config['DATABASE'] = getenv('DATABASE');
+        self::$config['PASSWORD'] = getenv('PASSWORD');
+    } 
+
+} 
+
+
+?>
